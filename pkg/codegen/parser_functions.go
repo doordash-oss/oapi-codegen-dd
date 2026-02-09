4f7712d4094b76328a854efb11e381756dc0e808
Igor Hittrich <igor.hittrich@wolt.com>
2026-02-09 18:58:38 +0100

    match json response in tpl

--- pkg/codegen/parser_functions.go
+++ pkg/codegen/parser_functions.go
@@ -36,6 +36,7 @@ var TemplateFunctions = template.FuncMap{
 	"fst":            fst,
 	"hasPrefix":      strings.HasPrefix,
 	"hasSuffix":      strings.HasSuffix,
+	"contains":       strings.Contains,
 	"str":            str,
 	"dict":           dict,
 	"slice":          func() []any { return []any{} },
